1ce907b5658518e952864e55cf4a47ab9b2a6985
sameh doush <samehdev95@gmail.com>
2023-07-24 15:37:07 +0300

    Update SubscriptionsServiceProvider.php

--- src/SubscriptionsServiceProvider.php
+++ src/SubscriptionsServiceProvider.php
@@ -5,6 +5,7 @@ namespace Samehdoush\Subscriptions;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use Samehdoush\Subscriptions\Commands\SubscriptionsCommand;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
 
 class SubscriptionsServiceProvider extends PackageServiceProvider
 {
@@ -18,8 +19,22 @@ class SubscriptionsServiceProvider extends PackageServiceProvider
         $package
             ->name('subscriptions')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_subscriptions_table')
-            ->hasCommand(SubscriptionsCommand::class);
+            // ->hasViews()
+            ->hasMigrations(['2023_07_21_000001_create_plans_table', '2023_07_21_000002_create_plan_features_table', '2023_07_21_000003_create_plan_subscriptions_table', '2023_07_21_000004_create_plan_subscription_usage'])
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command
+                    ->startWith(function (InstallCommand $command) {
+                        $command->info('Hello, and welcome to my great new package!');
+                    })
+                    ->publishConfigFile()
+                    ->publishMigrations()
+                    ->askToRunMigrations()
+                    // ->copyAndRegisterServiceProviderInApp()
+                    ->askToStarRepoOnGitHub('samehdoush/subscriptions')
+                    ->endWith(function (InstallCommand $command) {
+                        $command->info('Have a great day!');
+                    });
+            });
+        // ->hasCommand(SubscriptionsCommand::class);
     }
 }
