440fee57f2eab3208f91053b6522ec22290de6a7
sameh doush <samehdev95@gmail.com>
2023-08-01 19:55:33 +0300

    Update 2023_07_21_000003_create_plan_subscriptions_table.php

--- database/migrations/2023_07_21_000003_create_plan_subscriptions_table.php
+++ database/migrations/2023_07_21_000003_create_plan_subscriptions_table.php
@@ -15,30 +15,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create(config('subscriptions.tables.plan_subscriptions'), function (Blueprint $table) {
-            $table->increments('id');
-            $table->morphs('subscriber');
-            $table->integer('plan_id')->unsigned();
-            $table->string('slug');
-            $table->json('name');
-            $table->json('description')->nullable();
-            $table->dateTime('trial_ends_at')->nullable();
-            $table->dateTime('starts_at')->nullable();
-            $table->dateTime('ends_at')->nullable();
-            $table->dateTime('cancels_at')->nullable();
-            $table->dateTime('canceled_at')->nullable();
-            $table->double('price')->nullable();
-            $table->string('currency')->nullable();
 
-            $table->string('timezone')->nullable();
-            $table->timestamps();
-            $table->softDeletes();
+        if (!Schema::hasTable(config('subscriptions.tables.plan_subscriptions'))) {
 
-            // Indexes
-            // $table->unique('slug');
-            $table->foreign('plan_id')->references('id')->on(config('subscriptions.tables.plans'))
-                ->onDelete('cascade')->onUpdate('cascade');
-        });
+            Schema::create(config('subscriptions.tables.plan_subscriptions'), $this->blueprint());
+        } else {
+            Schema::table(config('subscriptions.tables.plan_subscriptions'), $this->blueprint());
+        }
     }
 
     /**
@@ -49,5 +32,94 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists(config('subscriptions.tables.plan_subscriptions'));
+    }
+
+
+    public function blueprint(): Closure
+    {
+
+        $TABLE = config('subscriptions.tables.plan_subscriptions');
+        return  function (Blueprint $table) use ($TABLE) {
+
+            if (!Schema::hasColumn($TABLE, 'id')) {
+                $table->increments('id');
+            }
+
+
+
+            if (!Schema::hasColumn($TABLE, 'subscriber_id')) {
+                $table->morphs('subscriber');
+            }
+
+
+            if (!Schema::hasColumn($TABLE, 'plan_id')) {
+                $table->integer('plan_id')->unsigned();
+            }
+
+            if (!Schema::hasColumn($TABLE, 'slug')) {
+                $table->string('slug');
+            }
+
+            if (!Schema::hasColumn($TABLE, 'name')) {
+                $table->json('name');
+            }
+
+            if (!Schema::hasColumn($TABLE, 'description')) {
+                $table->json('description')->nullable();
+            }
+
+            if (!Schema::hasColumn($TABLE, 'trial_ends_at')) {
+                $table->dateTime('trial_ends_at')->nullable();
+            }
+
+            if (!Schema::hasColumn($TABLE, 'starts_at')) {
+                $table->dateTime('starts_at')->nullable();
+            }
+
+            if (!Schema::hasColumn($TABLE, 'ends_at')) {
+                $table->dateTime('ends_at')->nullable();
+            }
+
+            if (!Schema::hasColumn($TABLE, 'cancels_at')) {
+                $table->dateTime('cancels_at')->nullable();
+            }
+
+            if (!Schema::hasColumn($TABLE, 'canceled_at')) {
+                $table->dateTime('canceled_at')->nullable();
+            }
+
+
+            if (!Schema::hasColumn($TABLE, 'price')) {
+                $table->double('price')->nullable();
+            }
+
+
+            if (!Schema::hasColumn($TABLE, 'currency')) {
+                $table->string('currency')->nullable();
+            }
+
+            if (!Schema::hasColumn($TABLE, 'timezone')) {
+                $table->string('timezone')->nullable();
+            }
+
+
+
+            if (!Schema::hasColumn($TABLE, 'created_at')) {
+                $table->timestamps();
+            }
+
+
+            if (!Schema::hasColumn($TABLE, 'deleted_at')) {
+                $table->softDeletes();
+            }
+
+
+            // Indexes
+            // $table->unique('slug');
+            if (!Schema::hasIndex($TABLE, 'plan_id')) {
+                $table->foreign('plan_id')->references('id')->on(config('subscriptions.tables.plans'))
+                    ->onDelete('cascade')->onUpdate('cascade');
+            }
+        };
     }
 };
